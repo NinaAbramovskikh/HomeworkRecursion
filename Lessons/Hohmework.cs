c95fe7d03cb3c18a9abe305a31126ccdf6731b25
Dmitry Abramovskikh <dab@colos.studio>
2024-07-10 12:23:37 +0300

    исправление

--- Lessons/Hohmework.cs
+++ Lessons/Hohmework.cs
@@ -60,7 +60,7 @@ namespace Lessons
                 return;
 
             Console.Write(numbers[numbers.Length - 1] + " ");
-            WriteReverce(numbers.Skip(1).ToArray());
+            WriteReverce(numbers.Take(numbers.Length - 2).ToArray());
         }
 
         private static int[] GetSequence(int first, int Second)
